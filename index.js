7d82babd4d28f2cff534560d5c66ea018873944d
MdShaharulIslam <shaharulislam025@gmail.com>
2024-05-15 23:15:16 +0600

    final repo added

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcvsatz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
